a6bfcc6eec9cd3e057285df0c7bd7128195c0bb7
veilhelmAlex <veilhelm.alexander@gmail.com>
2020-10-08 00:55:41 -0500

    added photos and submit button to register form

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,6 +1,13 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { MorphicInput } from "../../components/Inputs"
 import "./Register.scss"
+import { FilePond, registerPlugin } from 'react-filepond'
+import 'filepond/dist/filepond.min.css'
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
+import MorphicButton from "../../components/morphicButton"
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
 
 
 const setUpRegisterView = () =>{
@@ -12,6 +19,8 @@ const dismountRegisterView = () =>{
 }
 
 export default function Register () {
+    const [files, setFiles] = useState([])
+
     useEffect(() =>{
         setUpRegisterView()
         return () => dismountRegisterView()
@@ -51,6 +60,18 @@ export default function Register () {
             placeHolder="phone-number"
             // value={}
             ></MorphicInput>
+            <FilePond
+            files={files}
+            onupdatefiles={setFiles}
+            allowMultiple={true}
+            maxFiles={3}
+            server="/api"
+            name="files"
+            labelIdle='drag a cool photo or <span class="filepond--label-action">search</span>'
+            />
+            <MorphicButton
+                className="register__submit-btn"
+            >submit</MorphicButton>
         </div>
     )
 }
